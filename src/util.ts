3ee6d03718c07e9c3fac36dceaf056fbdacb96c4
maribbit <maribbit@outlook.com>
2025-03-22 23:05:32 +0800

    remove comment

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,3 @@
-// src/util.ts
 export function isEmpty(value: unknown): boolean {
-    return value === undefined || value === null || Number.isNaN(value);
-  }
+  return value === undefined || value === null || Number.isNaN(value);
+}
